64751af2687bd27799dcaac1fd290db87c4e378c
Abdul03-belendung <abdulazziz0311@gmail.com>
2023-09-07 09:43:25 +0700

    Tambah Tugas

---
+++ Tugas1.py
@@ -0,0 +1,9 @@
+# Assignment 1
+#Write a program to change vocal word ("a,i,u,e,o") into "a" .Example:
+
+str_val = "balonku ada lima"
+str_val = str_val.replace("o", "a")
+str_val = str_val.replace("u", "a")
+str_val = str_val.replace("i", "a")
+
+print(str_val)
